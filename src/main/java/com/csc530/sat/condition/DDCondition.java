894bd9d7f14a084c59d4af9b24338fc22a47efd5
bbeck13 <bbeck13@ischool.org>
2017-06-05 17:04:35 -0700

    updated DDConditionComents

--- src/main/java/com/csc530/sat/condition/DDCondition.java
+++ src/main/java/com/csc530/sat/condition/DDCondition.java
@@ -2,12 +2,38 @@ package com.csc530.sat.condition;
 
 import com.csc530.sat.type.DDType;
 
+/**
+ * 
+ * A condition over a type
+ *
+ * @param <T>
+ *            the type class this condition is over
+ */
 public interface DDCondition<T> {
+    /**
+     * 
+     * @param value
+     * @return true if the value satisfies the condition
+     */
     public boolean satisifies(DDType<T> value);
 
+    /**
+     * 
+     * @return a value that satisfies this condition null if no value satisfies
+     *         it
+     */
     public DDType<T> satisifier();
 
+    /**
+     * 
+     * @return a value that does not satisfy this condition null if no value
+     *         does not satisfy it
+     */
     public DDType<T> unSatisifier();
 
+    /**
+     * 
+     * @return the inverse of this condition
+     */
     public DDCondition<T> not();
 }
